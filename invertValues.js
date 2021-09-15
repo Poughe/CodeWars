73a8727adf30c83e6765e0236062603d21daa784
Ernest Pough <poughe@gmail.com>
2021-09-15 09:11:13 -0400

    codeWars

---
+++ invertValues.js
@@ -0,0 +1,7 @@
+function invert(array) {
+    const arr=[];
+   for (let i = 0; i < array.length; i++) { arr.push(-array[i]);}
+   return arr;
+ }
+
+//https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/solutions/javascript
