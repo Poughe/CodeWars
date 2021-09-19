bd5de9a227241501ebe4e1787672bf0c2f8ade2e
Ernest Pough <poughe@gmail.com>
2021-09-18 21:04:56 -0400

    codeWars

---
+++ removeFirstAndLastChar.js
@@ -0,0 +1,7 @@
+function removeChar(str){
+    //You got this!
+    const res = str.slice(1, -1);
+    return res;
+   };
+   
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/solutions/javascript
