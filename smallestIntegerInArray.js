825a5fa6165a58d05f8ca3bbbcd01df8f77d814c
Ernest Pough <poughe@gmail.com>
2021-09-19 23:37:03 -0400

    codeWars

---
+++ smallestIntegerInArray.js
@@ -0,0 +1,8 @@
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+    return Math.min(...args)
+  }
+    
+  }
+
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/solutions/javascript
