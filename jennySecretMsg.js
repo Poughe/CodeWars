06e581979b7574fd34238746f41704b9a14e08b4
Ernest Pough <poughe@gmail.com>
2021-09-21 09:14:21 -0400

    codeWars

---
+++ jennySecretMsg.js
@@ -0,0 +1,5 @@
+function greet(name){
+    return (name === "Johnny" ? `Hello, my love!` : `Hello, ${name}!`) 
+    }
+
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/javascript
