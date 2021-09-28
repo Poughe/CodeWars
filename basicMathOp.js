5176743e22e55bad62678fd55f0591a21dc5650c
Ernest Pough <poughe@gmail.com>
2021-09-28 16:18:55 -0400

    codeWars

---
+++ basicMathOp.js
@@ -0,0 +1,3 @@
+let basicOp = (operation, value1, value2) => { return eval(`${value1}` + `${operation}` + `${value2}`) }
+
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/solutions/javascript
