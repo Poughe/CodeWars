3c96fd12e35f02bd2322378967cd1aa497274455
Ernest Pough <poughe@gmail.com>
2021-09-29 09:29:44 -0400

    codeWars

---
+++ watermelon.js
@@ -0,0 +1,11 @@
+function divide(weight){
+    //your code here
+    return (weight >= 4 && weight % 2 === 0)
+  }
+
+  function divide(weight){
+    return weight < 4 ? false : weight % 2 === 0 ? true : false;
+  }
+
+
+//https://www.codewars.com/kata/55192f4ecd82ff826900089e/solutions/javascript
