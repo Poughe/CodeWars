34f597b4ffffb07f1d8b6756c6b0308490190618
Ernest Pough <poughe@gmail.com>
2021-10-05 09:43:18 -0400

    codeWars

---
+++ countByX.js
@@ -0,0 +1,9 @@
+function countBy(x, n) {
+    const z=[];
+    for(let i = x; i <= x*n; i+=x) {
+         z.push(i);
+    }
+    return z
+    }
+
+//https://www.codewars.com/kata/5513795bd3fafb56c200049e/solutions/javascript
