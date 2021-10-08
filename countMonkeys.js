f470321786edcba32d34b79f1c8c439afe6d4bf7
Ernest Pough <poughe@gmail.com>
2021-10-07 22:39:45 -0400

    codeWars

---
+++ countMonkeys.js
@@ -0,0 +1,9 @@
+function monkeyCount(n) {
+    const Monkey = []
+    for (let i = 1; i <= n; i++) {
+        Monkey.push(i);
+    }
+    return Monkey
+}
+
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/solutions/javascript
