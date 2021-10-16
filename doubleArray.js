b3f448e0b424bc62fc947cf6092fd2f8212b91ef
Ernest Pough <poughe@gmail.com>
2021-10-16 18:23:39 -0400

    given array, return new array with values doubled

---
+++ doubleArray.js
@@ -0,0 +1,9 @@
+function maps(x) {
+    let newArr = [];
+    for (let i = 0; i < x.length; i++) {
+        newArr.push(x[i] * 2);
+    }
+    return newArr;
+}
+
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e/solutions/javascript
