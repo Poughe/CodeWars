1e470bb57f00bd362e1b4cab2fa675827d8b2388
Ernest Pough <poughe@gmail.com>
2021-10-22 10:11:04 -0400

    codeWars

---
+++ reverseWord.js
@@ -0,0 +1,5 @@
+function reverseWords(str) {
+    return str.split(' ').reverse().join(' '); // reverse those words
+}
+
+//https://www.codewars.com/kata/51c8991dee245d7ddf00000e/solutions/javascript
