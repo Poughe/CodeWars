8a18260273dcdddd0b8717eb5dfdead0a7872440
Ernest Pough <poughe@gmail.com>
2021-10-30 10:37:14 -0400

    codeWars

---
+++ playingBanjo.js
@@ -0,0 +1,5 @@
+function areYouPlayingBanjo(name) {
+    return name + (name[0] === 'r' || name[0] === 'R' ? ' plays banjo' : ' does not play banjo');
+}
+
+//https://www.codewars.com/kata/53af2b8861023f1d88000832/solutions/javascript
