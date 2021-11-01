c44af2a2819bb99bb03ed3264699a496c5a04219
Ernest Pough <poughe@gmail.com>
2021-11-01 12:02:51 -0400

    codeWars

---
+++ 6kyu/findUniqNum.js
@@ -0,0 +1,6 @@
+function findUniq(arr) {
+    arr.sort((a, b) => a - b)
+    return arr[0] == arr[1] ? arr.pop() : arr[0]
+}
+
+//https://www.codewars.com/kata/585d7d5adb20cf33cb000235/solutions/javascript
