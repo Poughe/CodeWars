114dd63f35f66692d364ec18882ca554ce7913cd
Ernest Pough <poughe@gmail.com>
2021-11-03 08:56:49 -0400

    codeWars

---
+++ sortAndStar.js
@@ -0,0 +1,7 @@
+function twoSort(s) {
+    const sorted = s.sort();
+
+    return sorted[0].split('').join('***');
+}
+
+//https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/solutions/javascript/following/best_practice
