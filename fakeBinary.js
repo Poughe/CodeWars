a45e429f18e8942bbdeb7dd31949b0ea16d2fd2f
Ernest Pough <poughe@gmail.com>
2021-11-04 08:58:54 -0400

    codeWars

---
+++ fakeBinary.js
@@ -0,0 +1,5 @@
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/solutions/javascript/following/best_practice
