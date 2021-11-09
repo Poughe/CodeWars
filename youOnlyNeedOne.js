3558a8fa27b6c28b8102dbd53ec2e02e7492b45a
Ernest Pough <poughe@gmail.com>
2021-11-09 09:03:21 -0500

    codeWars

---
+++ youOnlyNeedOne.js
@@ -0,0 +1,11 @@
+function check(a, x) {
+    // your code here
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === x) {
+            return true
+        }
+    }
+    return false
+}
+
+//https://www.codewars.com/kata/57cc975ed542d3148f00015b/solutions/javascript/following/best_practice
