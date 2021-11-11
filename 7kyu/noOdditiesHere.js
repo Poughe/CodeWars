2569f785ab5bef3ca63a65c6c0ec2e7b2450eb25
Ernest Pough <poughe@gmail.com>
2021-11-11 09:14:05 -0500

    returns the values of an array that are not odd

---
+++ 7kyu/noOdditiesHere.js
@@ -0,0 +1,6 @@
+function noOdds(values) {
+    // Return all non-odd values
+    return values.filter(val => val % 2 === 0);
+}
+
+//https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/solutions/javascript
