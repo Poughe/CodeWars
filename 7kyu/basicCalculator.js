bb2a645a8b13f911aad3adb9477350efe6373555
Ernest Pough <poughe@gmail.com>
2021-11-14 11:22:23 -0500

    codeWars

---
+++ 7kyu/basicCalculator.js
@@ -0,0 +1,16 @@
+function calculate(num1, operation, num2) {
+    if (num2 === 0 && operation === '/') { return null } else {
+        switch (operation) {
+            case '+': return num1 + num2;
+                break;
+            case '-': return num1 - num2
+                break;
+            case '*': return num1 * num2
+                break;
+            case '/': return num1 / num2;
+            default: return null;
+        }
+    }
+}
+
+//https://www.codewars.com/kata/5296455e4fe0cdf2e000059f/solutions/javascript
