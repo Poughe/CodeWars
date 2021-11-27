1765b37acdb06680e11915d1bbda2d97edfde235
Ernest Pough <poughe@gmail.com>
2021-11-27 17:09:44 -0500

    codeWars

---
+++ feastManyBeasts.js
@@ -0,0 +1,6 @@
+function feast(beast, dish) {
+    return (beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1] ? true : false)
+
+}
+
+//https://www.codewars.com/kata/5aa736a455f906981800360d/solutions/javascript
