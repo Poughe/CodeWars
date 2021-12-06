203709c10dadcd913a1f81d6a553a06193e2e365
Ernest Pough <poughe@gmail.com>
2021-12-06 10:59:30 -0500

    codeWars

---
+++ differenceinAge.js
@@ -0,0 +1,5 @@
+function differenceInAges(ages) {
+    return [Math.min(...ages), Math.max(...ages), Math.max(...ages) - Math.min(...ages)]
+}
+
+//https://www.codewars.com/kata/5720a1cb65a504fdff0003e2/solutions/javascript
