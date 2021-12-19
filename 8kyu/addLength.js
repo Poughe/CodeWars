50f1b7a3ada03cf4d7c2b53de8d795e82e62df46
Ernest Pough <poughe@gmail.com>
2021-12-19 17:49:46 -0500

    codeWars

---
+++ 8kyu/addLength.js
@@ -0,0 +1,5 @@
+function addLength(str) {
+    return str.split(' ').map(function (v) { return v + ' ' + v.length })
+}
+
+//https://www.codewars.com/kata/559d2284b5bb6799e9000047/solutions/javascript
