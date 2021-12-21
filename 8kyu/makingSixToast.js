4074efd0d786be84808dcf9491a0aae544510dd1
Ernest Pough <poughe@gmail.com>
2021-12-21 14:19:07 -0500

    codeWars

---
+++ 8kyu/makingSixToast.js
@@ -0,0 +1,8 @@
+function sixToast(num) {
+    if (num < 6) {
+        return 6 - num
+    } else
+        return num - 6;
+}
+
+//https://www.codewars.com/kata/5834fec22fb0ba7d080000e8/solutions/javascript
