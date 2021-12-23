862392f33dac241e546c62212be434ed8540a0bc
Ernest Pough <poughe@gmail.com>
2021-12-23 09:27:03 -0500

    codeWars

---
+++ 8kyu/baryCenterTriangle.js
@@ -0,0 +1,3 @@
+let barTriang = (p1, p2, p3) => [+((p1[0] + p2[0] + p3[0]) / 3).toFixed(4), +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4)]
+
+//https://www.codewars.com/kata/5601c5f6ba804403c7000004/solutions/javascript
