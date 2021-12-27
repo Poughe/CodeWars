5ad47085676d3020d46775d702c99aee83607c80
Ernest Pough <poughe@gmail.com>
2021-12-27 09:41:33 -0500

    codeWars

---
+++ 8kyu/collatzConjecture.js
@@ -0,0 +1,9 @@
+var hotpo = function (n, acc = 0) {
+    if (n <= 1) {
+        return acc;
+    } else {
+        return hotpo(n % 2 == 0 ? n / 2 : 3 * n + 1, acc + 1);
+    }
+}
+
+//https://www.codewars.com/kata/577a6e90d48e51c55e000217/solutions/javascript
