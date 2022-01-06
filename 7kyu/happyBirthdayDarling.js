cfedb53534d00f0aeb69788cccf57abf20f879be
Ernest Pough <poughe@gmail.com>
2022-01-05 19:38:20 -0500

    codeWars

---
+++ 7kyu/happyBirthdayDarling.js
@@ -0,0 +1,5 @@
+function womensAge(n) {
+    return n % 2 ? `${n}? That's just 21, in base ${Math.floor(n / 2)}!` : `${n}? That's just 20, in base ${n / 2}!`
+}
+
+//https://www.codewars.com/kata/5e96332d18ac870032eb735f/solutions/javascript
