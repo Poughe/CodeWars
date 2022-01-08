02ff9de473b393783cf54b83c6855aafab63fa15
Ernest Pough <poughe@gmail.com>
2022-01-07 20:07:22 -0500

    codeWars

---
+++ 8kyu/whatIsBetween.js
@@ -0,0 +1,10 @@
+function between(a, b) {
+    let ar = []
+    while (a <= b) {
+        ar.push(a)
+        a++
+    }
+    return ar
+}
+
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029/solutions/javascript
