165cec73e8dffdddf8efb81aa0b03e61e323b130
Ernest Pough <poughe@gmail.com>
2022-01-14 20:03:52 -0500

    codeWars

---
+++ 7kyu/twoFightersOneWinner.js
@@ -0,0 +1,24 @@
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    //need a variable that take turns on whos attacking
+    let turn = fighter1.name == firstAttacker ? fighter1.name : fighter2.name
+
+    while (fighter1.health > 0 && fighter2.health > 0) {
+        if (turn == fighter1.name) {
+            fighter2.health -= fighter1.damagePerAttack
+            if (fighter2.health <= 0) {
+                return turn
+            }
+            turn = fighter2.name
+        }
+        if (turn == fighter2.name) {
+            fighter1.health -= fighter2.damagePerAttack
+            if (fighter1.health <= 0) {
+                return turn
+            }
+            turn = fighter1.name
+        }
+
+    }
+}
+
+//https://www.codewars.com/kata/577bd8d4ae2807c64b00045b/solutions/javascript
