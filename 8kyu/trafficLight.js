e2f16be9674eed1f10905c127a8a331a9b820f3e
Ernest Pough <poughe@gmail.com>
2022-01-15 20:05:27 -0500

    codeWars

---
+++ 8kyu/trafficLight.js
@@ -0,0 +1,7 @@
+function updateLight(current) {
+
+    return current === 'green' ? current = 'yellow' : current === 'yellow' ? current = 'red' : current === 'red' ? current = 'green' : null
+
+}
+
+//https://www.codewars.com/kata/58649884a1659ed6cb000072/solutions/javascript
