9394501a78ded395801a4065bb56702f2e8c1470
Ernest Pough <poughe@gmail.com>
2022-01-27 19:44:57 -0500

    codeWars

---
+++ 7kyu/greetMe.js
@@ -0,0 +1,5 @@
+var greet = function (name) {
+    return `Hello ${name[0].toUpperCase()}${name.toLowerCase().slice(1)}!`
+};
+
+//https://www.codewars.com/kata/535474308bb336c9980006f2/solutions/javascript
